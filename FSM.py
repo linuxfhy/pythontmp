3f3e5dd0d345f9779b2312e17d5d921d9555d32c
linuxfhy <linuxfhy@163.com>
2017-08-19 17:25:59 +0800

    update FSM.py

--- FSM.py
+++ FSM.py
@@ -5,9 +5,11 @@ FSM_TRANS_TABLE = [
     {'trigger': 'to_chifan', 'source': 'dadoudou', 'dest': 'chifan'}]
 
 class FsmStateTrans:
-    def __init__(self, srcstate, desstate):
+    def __init__(self, srcstate, desstate, trigger):
         self.srcstate = srcstate #源状态
         self.desstate = desstate #目标状态
+        self.trigger = trigger
+
 class FsmState:
     def __init__(self, name):
         self.name = name
@@ -15,13 +17,23 @@ class FsmState:
     def add_trans(self, trans): #funciton for add trans
         self.trans.append(trans)
 
-statelist = []
-for trans in FSM_TRANS_TABLE:
-    transcase = FsmStateTrans(trans['source'], trans['dest'])
-    statecase = FsmState(trans['source'])
-    statecase.add_trans(transcase)
-    statelist.append(statecase)
+def find_state_by_name(state_list, name):
+    print('name is %s' %name)
+    for cur_state in state_list:
+        if cur_state.name == name
+            return cur_state
+    return None
 
-for states in statelist:
+STATE_LIST = []
+for trans in FSM_TRANS_TABLE:
+    transcase = FsmStateTrans(trans['source'], trans['dest'], trans['trigger'])
+#    statecase = FsmState(trans['source']) #todo:avoid repeated state in FSM_TRANS_TABLE
+    statecase = find_state_by_name(STATE_LIST, trans['source'])
+    if statecase == None:
+
+    statecase.add_trans(transcase)
+    STATE_LIST.append(statecase)
+
+for states in STATE_LIST:
     for trans in states.trans:
-        print ('trans src is %s, trans des is %s' %(trans.srcstate, trans.desstate))
+        print ('trans src is %s, trans des is %s,trig is %s' %(trans.srcstate, trans.desstate, trans.trigger))
